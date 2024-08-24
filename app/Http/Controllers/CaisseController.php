42404d6a46cccaefe7a1e07420d4e8c3c9dfaf67
sasfiboualem <bousasfi@gmail.com>
2024-08-24 20:25:41 +0100

    caisse filtrage

--- app/Http/Controllers/CaisseController.php
+++ app/Http/Controllers/CaisseController.php
@@ -10,32 +10,32 @@ use App\Models\Produit;
 
 class CaisseController extends Controller
 {
-    public function caisse(Request $request , $id){
-        // $categorys = Category::all();
-        // $produits = Produit::all();
-        // $informtions = Information::first();
+    public function caisse()
+    {
+        $categorys = Category::all();
+        $produits = Produit::all();
+        $informtions = Information::first();
 
-        // return view('caisse.test',['categorys'=>$categorys , 'informations'=>$informtions , 'produits'=>$produits]);
+        return view('caisse.test', ['categorys' => $categorys, 'informations' => $informtions, 'produits' => $produits]);
+    }
 
-        // ajax 
-    
-    //   $categoryId = $id;
-      $categoryId = 1;
-    
-            // Récupérer les produits en fonction de la catégorie sélectionnée
-            if ($categoryId) {
-                $produits = Produit::where('categorie_id', $categoryId)->get();
-            } else {
-                $produits = Produit::all();
-            }
-    
-            $categorys = Category::all();
-    
-            if ($request->ajax()) {
-                return response()->json(['produits' => $produits]);
-            }
-    
-            return view('caisse.test', ['produits' => $produits, 'categorys' => $categorys]);
-        
+    public function filtrage_des_produits($id)
+    {
+        try {
+
+            $categoryId = $id;
+
+            $produits = Produit::where('categorie_id', $categoryId)->get();
+
+            // Retournez les produits au format JSON
+            return response()->json(['produits' => $produits]);
+
+        } catch (\Exception $e) {
+            // Loguer l'exception pour le débogage
+            \Log::error($e);
+
+            // Retourner une réponse d'erreur
+            return response()->json(['error' => 'Internal Server Error'], 500);
+        }
     }
 }
