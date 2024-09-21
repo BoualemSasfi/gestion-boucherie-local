3eb1e70499a8e11eb4ca7213da2e83a1e440adf2
sasfiboualem <bousasfi@gmail.com>
2024-09-21 14:31:37 +0100

    2eme commit

--- app/Http/Controllers/CaisseController.php
+++ app/Http/Controllers/CaisseController.php
@@ -10,6 +10,9 @@ use App\Models\Magasin;
 use App\Models\Stock;
 use App\Models\Lestock;
 
+use Illuminate\Support\Facades\DB;
+
+
 
 class CaisseController extends Controller
 {
@@ -29,15 +32,23 @@ class CaisseController extends Controller
     {
         $id_magasin = 2;
         $magasin = Magasin::find($id_magasin);
-        $stock_frais = Stock::where('magasin_id',$id_magasin)->where('type','Frais')->first();
+        $stock_frais = Stock::where('magasin_id', $id_magasin)->where('type', 'Frais')->first();
         $id_stock = $stock_frais->id;
-        $lestocks = Lestock::where('stock_id',$id_stock)->get();
+        $lestocks = Lestock::where('stock_id', $id_stock)->get();
         // hadi tafichi man stock magasin 
-        $categorys = Lestock::where('stock_id',$id_stock)->join('categories')->where('categories.id','=','lestocks.category_id')
-        ->groupby('categorie_id');
+        $categories = Lestock::where('stock_id', $id_stock)
+        ->join('categories', 'categories.id', '=', 'lestocks.categorie_id')
+        ->select(
+            DB::raw('MIN(lestocks.id) as id_lestock'),  // Obtenir la valeur minimum de id_lestock
+            'lestocks.categorie_id as id',               // Grouper par categorie_id (alias id)
+            DB::raw('MIN(categories.nom) as nom'),       // Utiliser MIN() ou MAX() pour les autres colonnes
+            DB::raw('MIN(categories.photo) as photo')
+        )
+        ->groupBy('id')  // Grouper par categorie_id (alias id)
+        ->get();
 
 
-        return view('caisse.paccino', ['categorys' => $categorys, 'magasin' => $magasin, 'produits' => $produits]);
+        return view('caisse.paccino', ['categorys' => $categories, 'magasin' => $magasin, 'produits' => $lestocks]);
     }
 
     public function filtrage_des_produits($id)
@@ -50,7 +61,6 @@ class CaisseController extends Controller
 
             // Retournez les produits au format JSON
             return response()->json(['produits' => $produits]);
-
         } catch (\Exception $e) {
             // Loguer l'exception pour le débogage
             \Log::error($e);
