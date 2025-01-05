c6cd550f0df9a472a92b8d76773c5d93bab55f24
XDAMIX <xsxdamixsx@gmail.com>
2025-01-05 18:50:31 +0100

    changemnt de sytle pour la page categorie index

--- resources/views/admin/categorie/index.blade.php
+++ resources/views/admin/categorie/index.blade.php
@@ -1,171 +1,151 @@
 @extends('layouts.admin')
 @section('content')
-    {{-- retour en arrière  --}}
-    <div class="container" id="titre-page">
-        <div class="row justify-content-between align-items-center">
-            <div class="col-2">
-                <a href="{{ url('/admin') }}" class="btn btn-dark"><i class="fas fa-arrow-left pr-1"></i><span
-                        class="btn-description">Retour</span></a>
-            </div>
-            <div class="col-8 text-center">
-                <h2>Liste des catégories de viande</h2>
-            </div>
-            <div class="col-2 text-right">
-                <a href="{{ url('/admin/category/add') }}" class="btn btn-success"><i
-                        class="fas fa-plus fa-xl pr-1"></i><span class="btn-description">Ajouter</span></a>
 
-            </div>
+{{-- Retour en arrière --}}
+<div class="container" id="titre-page">
+    <div class="row justify-content-between align-items-center">
+        <div class="col-12 col-md-2 mb-2 mb-md-0">
+            <a href="{{ url('/admin') }}" class="btn btn-dark w-100"><i class="fas fa-arrow-left pr-1"></i><span
+                    class="btn-description">Retour</span></a>
         </div>
-
-    </div>
-
-
-
-
-    {{-- --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- --}}
-
-    <div class="container" style="margin-top: 10px;">
-        <div class="row animate__animated animate__backInLeft">
-
-            @foreach ($categorys as $category)
-                <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-                    <div class="card shadow mb-4">
-
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-12">
-                                    <img style="height: 250px;" class="card-img-top" src="{{ asset('storage/' . $category->photo) }}"
-                                        alt="">
-                                </div>
-                                <div class="col-12">
-                                    <h3 class="text-center">{{ $category->nom }}</h3>
-                                </div>
-                            </div>
-
-
-
-                            <div class="row justify-content-center text-center">
-
-                                <!-- button modifier -->
-                                <div class="col-6">
-                                    <form class="edit-form" action="" data-id="{{ $category->id }}"
-                                        data-name="{{ $category->nom }}" method="put">
-                                        @csrf
-                                        <button type="button" onclick="edit_confirmation(this)"
-                                            class="btn btn-warning p-2"><i
-                                                class="fas fa-pen fa-lg mr-2"></i> <span
-                                                class="btn-description">Modifier</span></button>
-                                    </form>
-                                </div>
-                                <!-- fin -->
-
-                                <!-- bouton supprimer  -->
-                                <div class="col-6">
-
-                                    {{-- delete button --}}
-                                    <form class="delete-form" action="" data-id="{{ $category->id }}"
-                                        data-name="{{ $category->titre }}" method="POST">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="button" onclick="supprimer_confirmation(this)"
-                                            class="btn btn-danger p-2"><i class="fas fa-times fa-lg mr-2"></i>
-                                            <span class="btn-description">Supprimer</span></button>
-                                    </form>
-
-                                </div>
-                                <!-- fin -->
-
-                            </div>
-
-
-                        </div>
-                    </div>
-                </div>
-            @endforeach
-
-
+        <div class="col-12 col-md-8 text-center">
+            <h2>Liste des catégories de viande</h2>
+        </div>
+        <div class="col-12 col-md-2 text-md-right mb-2 mb-md-0">
+            <a href="{{ url('/admin/category/add') }}" class="btn btn-success w-100"><span
+                    class="btn-description">Ajouter</span><i class="fas fa-plus fa-xl pr-1"></i></a>
         </div>
     </div>
+</div>
 
+{{-- Contenu principal --}}
+<div class="container mt-3">
+    <div class="d-flex justify-content-center align-items-center flex-wrap">
+        <div class="row w-100 animate__animated animate__backInLeft shadow-gray-500">
+            <div class="table-responsive">
+                <table id="produitsTable" class="table table-striped table-bordered w-100">
+                    <thead>
+                        <tr class="text-center">
+                            <th>Photo</th>
+                            <th>Titre</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody class="text-center">
+                        @foreach ($categorys as $category)
+                            <tr>
+                                <!-- Colonne Photo -->
+                                <td class="align-middle">
+                                    <img src="{{ asset('storage/' . $category->photo) }}" class="img-fluid"
+                                        style="height: 80px;" alt="Photo de la catégorie">
+                                </td>
 
-{{-- ---------------------------------------------------  --}}
+                                <!-- Colonne Titre -->
+                                <td class="align-middle">{{ $category->nom }}</td>
 
+                                <!-- Colonne Actions -->
+                                <td class="align-middle">
+                                    <div class="d-flex justify-content-between gap-2 flex-wrap">
+                                        <!-- Bouton Modifier -->
+                                        <form class="edit-form" action="/admin/category/{{ $category->id }}/edit"
+                                            data-id="{{ $category->id }}" method="GET">
+                                            @csrf
+                                            <button type="button" class="btn btn-link p-0"
+                                                onclick="edit_confirmation(this)">
+                                                <i class="fa-solid fa-pen fa-xl text-primary"></i>
+                                            </button>
+                                        </form>
 
-    {{-- script suppression  --}}
-    <script>
-        function supprimer_confirmation(button) {
-            // Utilisez le bouton pour obtenir le formulaire parent
-            const form = button.closest('.delete-form');
+                                        <!-- Bouton Supprimer -->
+                                        <form class="delete-form" action="" data-id="{{ $category->id }}"
+                                            data-name="{{ $category->titre }}" method="POST">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="button" class="btn btn-link p-0"
+                                                onclick="supprimer_confirmation(this)">
+                                                <i class="fa-solid fa-trash-can fa-xl text-danger"></i>
+                                            </button>
+                                        </form>
+                                    </div>
 
-            // Vérifiez si le formulaire a été trouvé
-            if (form) {
-                // Utilisez le formulaire pour extraire l'ID
-                const id = form.dataset.id;
-                const name = form.dataset.name;
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
 
-                Swal.fire({
-                    title: "Êtes-vous sûr(e) de vouloir supprimer cette catégorie ..?",
-                    text: name,
-                    icon: "question",
-                    showCancelButton: true,
-                    confirmButtonColor: "#198754",
-                    cancelButtonColor: "#d33",
-                    confirmButtonText: "Oui, Supprime-le",
-                    cancelButtonText: "Non, Annuler",
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        // Mettez à jour l'action du formulaire avec l'ID et soumettez-le
-                        form.action = `/admin/category/${id}/delete`;
-                        form.submit();
-
-                        Swal.fire({
-                            title: "Catégorie supprimée !",
-                            icon: "success"
-                        });
-                    }
-                });
-            } else {
-                console.error("Le formulaire n'a pas été trouvé.");
+{{-- Scripts pour les fonctionnalités --}}
+<script>
+    $(document).ready(function () {
+        $('#produitsTable').DataTable({
+            "pageLength": 10,
+            "lengthMenu": [[10, 20, 50], [10, 20, 50]],
+            "scrollY": "400px",
+            "scrollCollapse": true,
+            "searching": true,
+            "language": {
+                "url": "//cdn.datatables.net/plug-ins/1.13.6/i18n/fr-FR.json"
             }
+        });
+    });
+
+    function supprimer_confirmation(button) {
+        const form = button.closest('.delete-form');
+        if (form) {
+            const id = form.dataset.id;
+            const name = form.dataset.name;
+
+            Swal.fire({
+                title: "Êtes-vous sûr(e) de vouloir supprimer cette catégorie ?",
+                text: name,
+                icon: "question",
+                showCancelButton: true,
+                confirmButtonColor: "#198754",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Oui, Supprime-le",
+                cancelButtonText: "Non, Annuler",
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    form.action = `/admin/category/${id}/delete`;
+                    form.submit();
+                    Swal.fire({
+                        title: "Catégorie supprimée !",
+                        icon: "success"
+                    });
+                }
+            });
+        } else {
+            console.error("Le formulaire n'a pas été trouvé.");
         }
-    </script>
+    }
 
+    function edit_confirmation(button) {
+        const form = button.closest('.edit-form');
+        if (form) {
+            const id = form.dataset.id;
 
-
-    {{-- script modifier  --}}
-    <script>
-        function edit_confirmation(button) {
-            // Utilisez le bouton pour obtenir le formulaire parent
-            const form = button.closest('.edit-form');
-
-            // Vérifiez si le formulaire a été trouvé
-            if (form) {
-                // Utilisez le formulaire pour extraire l'ID
-                const id = form.dataset.id;
-                const name = form.dataset.name;
-
-                Swal.fire({
-                    title: "Êtes-vous sûr(e) de vouloir modifier cette categorie ..?",
-                    text: name,
-                    icon: "question",
-                    showCancelButton: true,
-                    confirmButtonColor: "#198754",
-                    cancelButtonColor: "#d33",
-                    confirmButtonText: "Oui",
-                    cancelButtonText: "Non",
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        // Mettez à jour l'action du formulaire avec l'ID et soumettez-le
-                        form.action = `/admin/category/${id}/edit`;
-                        form.submit();
-                    }
-                });
-            } else {
-                console.error("Le formulaire n'a pas été trouvé.");
-            }
+            Swal.fire({
+                title: "Êtes-vous sûr(e) de vouloir modifier cette catégorie ?",
+                icon: "question",
+                showCancelButton: true,
+                confirmButtonColor: "#198754",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Oui",
+                cancelButtonText: "Non",
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    form.action = `/admin/category/${id}/edit`;
+                    form.submit();
+                }
+            });
+        } else {
+            console.error("Le formulaire n'a pas été trouvé.");
         }
-    </script>
-
-
+    }
+</script>
 
 @endsection
